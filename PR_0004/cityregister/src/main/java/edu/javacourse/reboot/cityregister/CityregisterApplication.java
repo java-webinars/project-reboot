6f7eac8acaf026d18cda898bc8a1fe00087f11e2
java-webinars <chestrick@yandex.ru>
2025-04-20 16:34:53 -0400

    Lesson 4

---
+++ PR_0004/cityregister/src/main/java/edu/javacourse/reboot/cityregister/CityregisterApplication.java
@@ -0,0 +1,13 @@
+package edu.javacourse.reboot.cityregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityregisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityregisterApplication.class, args);
+	}
+
+}
